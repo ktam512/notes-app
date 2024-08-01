1fff95a617bf4d2534d6623fe1d90b37b56aea3a
ktam512 <ktam512@gmail.com>
2024-08-01 18:22:15 +0700

    Add the "delete note" function to backend

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -39,12 +39,17 @@ app.put("/api/notes/:id", async (req,res) =>{
     const {title, content} = req.body;
     const id = parseInt(req.params.id);
 
+    if(!title || !content){
+        return res
+        .status(400)
+        .send("title and content fields required");
+    }
+
     if (!id || isNaN(id)){
         return res
         .status(400)
         .send("ID must be a valid number");
     }
-
     try {
         const updatedNote = 
             await prisma.note.update({
@@ -57,6 +62,31 @@ app.put("/api/notes/:id", async (req,res) =>{
         .status(500)
         .send("Oops, something went wrong")
     }
+});
+
+app.delete("/api/notes/:id", async (req,res) =>{
+    const id = parseInt(req.params.id);
+
+    if (!id || isNaN(id)){
+        return res
+        .status(400)
+        .send("ID must be a valid number");
+    }
+
+    try {
+        await prisma.note.delete({
+            where: {id}
+        });
+        res
+        .status(204)
+        .send();
+    } catch (error) {
+        res
+        .status(500)
+        .send("Oops, something went wrong")
+    }
+
+
 });
 
 app.listen(4000, () => {
