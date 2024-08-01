098031cb85b00b5973f2100e5cabdbc4aecadc40
ktam512 <ktam512@gmail.com>
2024-08-01 17:24:57 +0700

    Add the backend "print out on console" function

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -1,12 +1,15 @@
 import express from "express"
 import cors from "cors"
+import { PrismaClient } from "@prisma/client";
 
 const app = express()
+const prisma = new PrismaClient();
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/api/notes", async (req, res) => {
+        const notes = await prisma.note.findMany();
     res.json({message: "success!"});
 });
 
