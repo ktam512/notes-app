8dd83887797abb1b9799f4a72e18932e15b5ed43
ktam512 <ktam512@gmail.com>
2024-08-01 18:16:03 +0700

    Add "edit note" function and covering for error

--- notes-app-server/src/index.ts
+++ notes-app-server/src/index.ts
@@ -15,10 +15,48 @@ app.get("/api/notes", async (req, res) => {
 
 app.post("/api/notes", async (req, res) => {
     const {title, content} = req.body;
-    const note = await prisma.note.create({
-        data: {title, content}
-    });
-    res.json(note);
+
+    if(!title || !content){
+        return res
+        .status(400)
+        .send("title and content fields required");
+    }
+    try {
+        const note = await prisma.note.create({
+            data: {title, content}
+        });
+        res.json(note);
+        
+    } catch (error) {
+        res
+        .status(500)
+        .send("Oops something went wrong")
+    }
+    
+});
+
+app.put("/api/notes/:id", async (req,res) =>{
+    const {title, content} = req.body;
+    const id = parseInt(req.params.id);
+
+    if (!id || isNaN(id)){
+        return res
+        .status(400)
+        .send("ID must be a valid number");
+    }
+
+    try {
+        const updatedNote = 
+            await prisma.note.update({
+                where: { id },
+                data: {title, content}
+            })
+            res.json(updatedNote)
+    } catch (error) {
+        res
+        .status(500)
+        .send("Oops, something went wrong")
+    }
 });
 
 app.listen(4000, () => {
